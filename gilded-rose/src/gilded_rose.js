fed960f06ab6abe2195e10c6de7093826471974e
Layla <laylamandella@gmail.com>
2022-01-31 13:05:02 -0800

    write tests, begin refactor

--- gilded-rose/src/gilded_rose.js
+++ gilded-rose/src/gilded_rose.js
@@ -1,3 +1,10 @@
+const ITEMS = {
+  Sulfuras: 'Sulfuras, Hand of Ragnaros',
+  AgedBrie: 'Aged Brie',
+  ConcertTicket: 'Backstage passes to a TAFKAL80ETC concert'
+};
+
+
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
@@ -12,42 +19,47 @@ class Shop {
   }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+
+      if (this.items[i].name !== ITEMS.AgedBrie && this.items[i].name !== ITEMS.ConcertTicket && this.items[i].name != ITEMS.Sulfuras) {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
+          // for non specialty items, decrease quality by 1
+          this.items[i].quality = this.items[i].quality - 1;     
         }
       } else {
+        // specialty items
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+          if (this.items[i].name === ITEMS.ConcertTicket) {
             if (this.items[i].sellIn < 11) {
               if (this.items[i].quality < 50) {
+                // increase quality by 1
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
+                // increase quality by 1
                 this.items[i].quality = this.items[i].quality + 1;
               }
             }
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+      if (this.items[i].name != ITEMS.Sulfuras) {
+        // decrease quality by 1
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].name !== ITEMS.AgedBrie) {
+          if (this.items[i].name !== ITEMS.ConcertTicket) {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name !== ITEMS.Sulfuras) {
+                // decrease quality by 1
                 this.items[i].quality = this.items[i].quality - 1;
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+            this.items[i].quality = 0;
           }
         } else {
           if (this.items[i].quality < 50) {
--- gilded-rose/test/gilded_rose.test.js
+++ gilded-rose/test/gilded_rose.test.js
@@ -2,28 +2,155 @@ const {Shop, Item} = require("../src/gilded_rose");
 
 const ItemType = {
   Sulfuras: 'Sulfuras, Hand of Ragnaros',
+  AgedBrie: 'Aged Brie',
+  ConcertTicket: 'Backstage passes to a TAFKAL80ETC concert',
+  CoolItem: "super cool item"
 };
 
-const sulfurasQuality = 80;
+const QUALITY = {
+  sulfurasQuality: 80,
+  agedBrieQuality: 2,
+  concertQuality: 5,
+  coolItemQuality: 20
+};
 
 describe("Gilded Rose", function() {
-  // Sulfuras item tests
-  // 
-
   it("should not modify the sell in of a Sulfuras item", function() {
-    const quality = sulfurasQuality;
+    const quality = QUALITY.sulfurasQuality;
     const sellIn = 5;
     let items = [new Item(ItemType.Sulfuras, sellIn, quality)];
     const gildedRose = new Shop(items);
-    for (let i = 0; i < 10; ++i) {
+    for (let i = 0; i < 10; i++) {
       items = gildedRose.updateQuality();
       expect(items[0].sellIn).toBe(sellIn);
     }
   });
 
-  // TODO: Add more tests
-  //
+  it("should increase the quality of the aged brie item by 1 up until sellIn === 0", function() {
+    let quality = QUALITY.agedBrieQuality;
+    const sellIn = 3;
+    let items = [new Item(ItemType.AgedBrie, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = sellIn; i > 0; i--) {
+      items = gildedRose.updateQuality();
+      quality = quality + 1;
+    }
+    expect(items[0].quality).toBe(quality);
+  });
 
+  it("should increase the quality of the aged brie item by 2 after sellIn has passed", function() {
+    let quality = QUALITY.agedBrieQuality;
+    const sellIn = 0;
+    let items = [new Item(ItemType.AgedBrie, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = 0; i > -3; i--) {
+      items = gildedRose.updateQuality();
+      quality = quality + 2;
+    }
+    expect(items[0].quality).toBe(quality);
+  });
+
+  it("should increase the quality of the concert ticket item by 2 until sellin is 5 or less", function() {
+    const quality = QUALITY.concertQuality;
+    const sellIn = 10;
+    let items = [new Item(ItemType.ConcertTicket, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = 0; i < sellIn-5; i++) {
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(quality + 2*(i+1));
+    }
+  });
+
+  it("should increase the quality of the concert ticket item by 1 until sellin is 10 or less", function() {
+    const quality = QUALITY.concertQuality;
+    const sellIn = 15;
+    let items = [new Item(ItemType.ConcertTicket, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = sellIn; i > 10; i--) {
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(quality + (sellIn - i) + 1);
+    }
+  });
+
+  it("should increase the quality of the concert ticket item by 3 when sellin is 5 or less", function() {
+    let quality = QUALITY.concertQuality;
+    const sellIn = 5;
+    let items = [new Item(ItemType.ConcertTicket, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = sellIn; i > 0; i--) {
+      items = gildedRose.updateQuality();
+      quality = quality + 3;
+      expect(items[0].quality).toBe(quality);
+    }
+  });
+
+  it("concert ticket item should have quality of 0 when sellin is 0 or less", function() {
+    let quality = QUALITY.concertQuality;
+    const sellIn = 0;
+    let items = [new Item(ItemType.ConcertTicket, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = sellIn; i > -3; i--) {
+      items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+    }
+  });
+
+  it("should degrade twice as fast once the sell by date has passed", function() {
+    let quality = QUALITY.coolItemQuality;
+    const sellIn = 0;
+    let items = [new Item(ItemType.CoolItem, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = 0; i > -3; i--) {
+      items = gildedRose.updateQuality();
+      if (quality > 1) {
+        quality = quality - 2;
+      } else {
+        quality = 0;
+      }
+      expect(items[0].quality).toBe(quality);
+    }
+  });
+
+  it("should degrade by 1 each day", function() {
+    let quality = QUALITY.coolItemQuality;
+    const sellIn = 10;
+    let items = [new Item(ItemType.CoolItem, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = sellIn; i > 0; i--) {
+      items = gildedRose.updateQuality();
+      if (quality > 0) {
+        quality = quality - 1;
+      }
+      expect(items[0].quality).toBe(quality);
+    }
+  });
+
+  it("should never have quality less than 0", function() {
+    let quality = QUALITY.coolItemQuality;
+    const sellIn = 0;
+    let items = [new Item(ItemType.CoolItem, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = sellIn; i > -30; i--) {
+      items = gildedRose.updateQuality();
+      
+      if (quality > 0) {
+        quality = quality - 2;
+      }
+    }
+    expect(items[0].quality).toBe(0);
+  });
+
+
+  it("should never have quality be more than 50", function() {
+    const quality = QUALITY.agedBrieQuality;
+    const sellIn = 0;
+    let items = [new Item(ItemType.AgedBrie, sellIn, quality)];
+    const gildedRose = new Shop(items);
+    for (let i = 0; i > -60; i--) {
+      items = gildedRose.updateQuality();
+    }
+    expect(items[0].quality).toBe(50);
+  });
 });
 
 // Example items
